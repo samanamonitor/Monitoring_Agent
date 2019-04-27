0caa87037de70fe5ca34ea7af6befd34c5cf4e77
acuba001 <4492431+acuba001@users.noreply.github.com>
2019-04-26 20:05:18 -0700

    Added initial bootstrap. Cleaned up Ajax script

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_pymongo import PyMongo
 from flask_hashing import Hashing
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,7 @@ hashing = Hashing(app)
 login = LoginManager(app)
 login.login_view = 'login'
 login.session_protection = 'strong'
+bootstrap = Bootstrap(app)
 
 # Add Pull defaults from a MongoDB collection
 app.config['PULL_DEFAULTS'] = mongo.db.defaultConfig.find_one(projection={'_id': False})
--- app/routes.py
+++ app/routes.py
@@ -135,6 +135,7 @@ def login():
     return render_template('login.html')
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
